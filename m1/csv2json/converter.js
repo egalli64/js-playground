3182c5fb638e9827a514b60a940c72ba2a9a875f
Emanuele Galli <egalli64@gmail.com>
2017-11-25 12:54:39 +0100

    User could pass its own filename

--- m1/csv2json/converter.js
+++ m1/csv2json/converter.js
@@ -1,53 +1,27 @@
-/*
-csvtojson https://npmjs.org/csvtojson
-
-from csv:
-id,first_name,last_name,email,gender,ip_address,ssn,credit_card,bitcoin,street_address
-1,Ario,Noteyoung,anoteyoung0@nhs.uk,Male,99.5.160.227,509-86-9654,5602256742685208,179BsXQkUuC6NKYNsQkdmKQKbMBPmJtEHB,0227 Kropf Court
-2,Minni,Endon,mendon1@netvibes.com,Female,213.62.229.103,765-11-9543,67613037902735554,135wbMcR98R6hqqWgEJXHZHcanQKGRPwE1,90 Sutteridge Way
-
-to json:
-[
-  {
-    "id": "1",
-    "first_name": "Ario",
-    "last_name": "Noteyoung",
-    "email": "anoteyoung0@nhs.uk",
-    "gender": "Male",
-    "ip_address": "99.5.160.227",
-    "ssn": "509-86-9654",
-    "credit_card": "5602256742685208",
-    "bitcoin": "179BsXQkUuC6NKYNsQkdmKQKbMBPmJtEHB",
-    "street_address": "0227 Kropf Court"
-  },
-  {
-    "id": "2",
-    "first_name": "Minni",
-    "last_name": "Endon",
-    "email": "mendon1@netvibes.com",
-    "gender": "Female",
-    "ip_address": "213.62.229.103",
-    "ssn": "765-11-9543",
-    "credit_card": "67613037902735554",
-    "bitcoin": "135wbMcR98R6hqqWgEJXHZHcanQKGRPwE1",
-    "street_address": "90 Sutteridge Way"
-  },
-  ...
+/**
+ * Convert a csv file to json
+ * 
+ * When no input filename provided, it looks for 'customer-data.csv'
+ * The output file has the same name but with '.json' extension
+ * Warning: if the output file already exists, it will be silently overwritten
  */
-
-const csvFilePath = 'customer-data.csv'
 const csv = require('csvtojson')
 const fs = require('fs')
-const path = require('path')
 
-var jsonArray = []
+const DEFAULT_FILENAME = 'customer-data.csv'
+const EXT = '.json'
+let jsonArray = []
 
-csv()
-  .fromFile(csvFilePath)
-  .on('json', (data) => {
-    //    console.log(data)
+const converter = (fileIn = DEFAULT_FILENAME) => {
+  let fileOut = fileIn.indexOf('.') == -1 ? fileIn + EXT : fileIn.replace(/\.[^/.]+$/, EXT)
+
+  csv().fromFile(fileIn).on('json', (data) => {
     jsonArray.push(data)
   }).on('end', () => {
-    fs.writeFileSync(path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonArray, null, 2))
-    //    console.log(jsonArray)
+    fs.writeFileSync(fileOut, JSON.stringify(jsonArray, null, 2))
+  }).on('error', (error) => {
+    console.error(`Can't convert ${fileIn}: ${error.message}`)
   })
+}
+
+converter(process.argv[2])
