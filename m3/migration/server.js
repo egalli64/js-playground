d04cb150d9c8f03ce13a1f8db27a5082c37760fe
Emanuele Galli <egalli64@gmail.com>
2017-12-02 12:53:04 +0100

    Code cleanup

--- m3/migration/server.js
+++ m3/migration/server.js
@@ -1,17 +1,18 @@
 const mongodb = require('mongodb')
 const async = require('async')
 
-const url = 'mongodb://localhost:27017/edx-course-db'
+const url = 'mongodb://localhost:27017/migration'
+const coll_name = 'customers'
 const customers = require('./m3-customer-data.json')
 const customerAddresses = require('./m3-customer-address-data.json')
 
 let tasks = []
 
 const x = parseInt(process.argv[2])
-const limit = (isNaN(x) || x < 0 || x > customers.length) ? customers.length : x
+const taskSize = (isNaN(x) || x < 0 || x > customers.length) ? customers.length : x
 
-if (customers.length % limit) {
-    console.log(`WARNING: the last ${customers.length % limit} customer(s) won't be generated!`)
+if (customers.length % taskSize) {
+    console.log(`WARNING: the last ${customers.length % taskSize} customer(s) won't be generated!`)
 }
 
 mongodb.MongoClient.connect(url, (error, db) => {
@@ -21,26 +22,26 @@ mongodb.MongoClient.connect(url, (error, db) => {
     }
 
     customers.forEach((customer, index) => {
-        customers[index] = Object.assign(customer, customerAddresses[index])
+        customer = Object.assign(customer, customerAddresses[index])
     })
 
-    for (let beg = 0, end = limit; end <= customers.length; beg += limit, end += limit) {
+    for (let beg = 0, end = taskSize; end <= customers.length; beg += taskSize, end += taskSize) {
         tasks.push((callback) => {
             console.log(`Processing [${beg}-${end}) in [0-${customers.length})`)
-            db.collection('customers').insert(customers.slice(beg, end), (error, results) => {
+            db.collection(coll_name).insert(customers.slice(beg, end), (error, results) => {
                 callback(error, results)
             })
         })
     }
 
     console.log(`Launching ${tasks.length} parallel task(s)`)
-    const startTime = Date.now()
+    const tZero = Date.now()
     async.parallel(tasks, (error, results) => {
         if (error) {
             console.error(error)
         }
         else {
-            console.log(`${results.length} task(s) executed in ${Date.now() - startTime} ms.`)
+            console.log(`${results.length} task(s) executed in ${Date.now() - tZero} ms.`)
         }
         db.close()
     })
